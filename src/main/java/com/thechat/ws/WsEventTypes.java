d42920ae442773e3119c46f618e34d842200dd1a
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2026-08-02 22:18:31 +0530

    fix types

--- src/main/java/com/thechat/ws/WsEventTypes.java
+++ src/main/java/com/thechat/ws/WsEventTypes.java
@@ -7,10 +7,14 @@ public final class WsEventTypes {
     public static final String PONG = "pong";
     public static final String ERROR = "error";
 
-    public static final String MESSAGE_SEND = "message_send";
-    public static final String MESSAGE_NEW = "message_new";
-    public static final String MESSAGE_UPDATED = "message_updated";
-    public static final String MESSAGE_DELETED = "message_deleted";
+    public static final String MESSAGE_SEND = "message_send"; // client send message under this key
+    public static final String MESSAGE_NEW = "message_new";// clients recieve message under this key
+
+    public static final String MESSAGE_UPDATE = "message_update"; // client send update message under this key
+    public static final String MESSAGE_UPDATED = "message_updated"; // clients recieve updated message under this key
+
+    public static final String MESSAGE_DELETE = "message_delete"; // client send delete message under this key
+    public static final String MESSAGE_DELETED = "message_deleted"; // clients recieve deleted message under this key
 
     private WsEventTypes() {
     }
