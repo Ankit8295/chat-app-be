8093379c4b17a212cc9023e770e459ffc01e793a
Ankit Kumar <112799319+Ankit8295@users.noreply.github.com>
2026-08-02 23:23:41 +0530

    added event type

--- src/main/java/com/thechat/ws/WsEventTypes.java
+++ src/main/java/com/thechat/ws/WsEventTypes.java
@@ -7,14 +7,23 @@ public final class WsEventTypes {
     public static final String PONG = "pong";
     public static final String ERROR = "error";
 
-    public static final String MESSAGE_SEND = "message_send"; // client send message under this key
-    public static final String MESSAGE_NEW = "message_new";// clients recieve message under this key
+    public static final String MESSAGE_SEND = "message_send";
+    public static final String MESSAGE_NEW = "message_new";
 
-    public static final String MESSAGE_UPDATE = "message_update"; // client send update message under this key
-    public static final String MESSAGE_UPDATED = "message_updated"; // clients recieve updated message under this key
+    public static final String MESSAGE_UPDATE = "message_update";
+    public static final String MESSAGE_UPDATED = "message_updated";
 
-    public static final String MESSAGE_DELETE = "message_delete"; // client send delete message under this key
-    public static final String MESSAGE_DELETED = "message_deleted"; // clients recieve deleted message under this key
+    public static final String MESSAGE_DELETE = "message_delete";
+    public static final String MESSAGE_DELETED = "message_deleted";
+
+    public static final String MEMBER_ADD = "member_add";
+    public static final String MEMBER_ADDED = "member_added";
+
+    public static final String MEMBER_REMOVE = "member_remove";
+    public static final String MEMBER_REMOVED = "member_removed";
+
+    public static final String GROUP_UPDATE = "group_update";
+    public static final String GROUP_UPDATED = "group_updated";
 
     private WsEventTypes() {
     }
